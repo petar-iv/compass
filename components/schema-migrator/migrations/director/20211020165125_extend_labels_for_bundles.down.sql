74ab7eaacfca4af2ae4f9c42165753f5beecd47e
Daniel Gospodinov <daniel.gospodinov@sap.com>
2021-10-26 23:16:29 +0300

    Extend migration to support coalesces

--- components/schema-migrator/migrations/director/20211020165125_extend_labels_for_bundles.down.sql
+++ components/schema-migrator/migrations/director/20211020165125_extend_labels_for_bundles.down.sql
@@ -1,5 +1,11 @@
 BEGIN;
 
+DROP INDEX IF EXISTS labels_tenant_id_key_coalesce_idx;
+CREATE UNIQUE INDEX labels_tenant_id_key_coalesce_coalesce1_coalesce2_idx ON labels (tenant_id, key,
+                                                                                     coalesce(app_id, '00000000-0000-0000-0000-000000000000'),
+                                                                                     coalesce(runtime_id, '00000000-0000-0000-0000-000000000000'),
+                                                                                     coalesce(labels.runtime_context_id, '00000000-0000-0000-0000-000000000000'));
+
 ALTER TABLE labels DROP CONSTRAINT labels_bundle_id_fk;
 ALTER TABLE labels DROP COLUMN bundle_id;
 
--- components/schema-migrator/migrations/director/20211020165125_extend_labels_for_bundles.up.sql
+++ components/schema-migrator/migrations/director/20211020165125_extend_labels_for_bundles.up.sql
@@ -6,4 +6,11 @@ ALTER TABLE labels
     ADD CONSTRAINT labels_bundle_id_fk
         FOREIGN KEY (bundle_id) REFERENCES bundles(id);
 
+DROP INDEX IF EXISTS labels_tenant_id_key_coalesce_coalesce1_coalesce2_idx;
+CREATE UNIQUE INDEX labels_tenant_id_key_coalesce_idx ON labels (tenant_id, key,
+                               coalesce(app_id, '00000000-0000-0000-0000-000000000000'),
+                               coalesce(runtime_id, '00000000-0000-0000-0000-000000000000'),
+                               coalesce(labels.runtime_context_id, '00000000-0000-0000-0000-000000000000'),
+                               coalesce(labels.bundle_id, '00000000-0000-0000-0000-000000000000'));
+
 COMMIT;
