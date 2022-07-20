d82d2d379094b773038e1464bf0b861d83debbe7
I331217 <gabriela.yordanova@sap.com>
2022-07-20 11:42:32 +0300

    Update destination repository skeleton

--- components/director/internal/domain/destination/repository.go
+++ components/director/internal/domain/destination/repository.go
@@ -1,1 +1,35 @@
 package destination
+
+import (
+	"context"
+
+	"github.com/kyma-incubator/compass/components/director/internal/repo"
+)
+
+const destinationTable = "public.destinations"
+
+var (
+	destinationColumns = []string{"id", "name", "type", "url", "authentication", "tenant_id", "bundle_id", "revision"}
+	conflictingColumns = []string{"id"}
+	updateColumns      = []string{"name", "type", "url", "authentication", "revision"}
+)
+
+type repository struct {
+	deleter  repo.Deleter
+	upserter repo.Upserter
+}
+
+func NewRepository() *repository {
+	return &repository{
+		deleter:  repo.NewDeleter(destinationTable),
+		upserter: repo.NewUpserter(destinationTable, destinationColumns, conflictingColumns, updateColumns),
+	}
+}
+
+func (r *repository) Upsert(ctx context.Context) error {
+	return nil
+}
+
+func (r *repository) Delete(ctx context.Context) error {
+	return nil
+}
